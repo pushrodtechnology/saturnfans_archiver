0ed3714f40e62f5d9590aeec4772831067298e00
Robert Cope <robert.cope@pushrodtechnology.com>
2015-04-16 18:45:31 -0600

    Checking in initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+__author__ = 'Robert Cope'
+
+from setuptools import setup
+
+setup(name='SaturnFans Scraper',
+      author=__author__,
+      author_email='robert.cope@pushrodtechnology.com',
+      version='0.0.1',
+      packages=['SaturnFansScraper'],
+      install_requires=['requests>=2.4.1'])
